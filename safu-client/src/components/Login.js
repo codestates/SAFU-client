20a3564f6b1ffe544b5883b6566e47b9f3389b43
Grace Chung <hjngy0511@gmail.com>
2020-10-19 22:34:45 +0900

    #7/:art:/login 구현/로그인 이벤트 구현 to do 2- 일반 로그인 구현 응답와 에러는 server 쪽 완료시 반영 계획 to do 3- 소셜 로그인을 위해 버튼에 소셜로그인 uri 넣음

--- safu-client/src/components/Login.js
+++ safu-client/src/components/Login.js
@@ -2,13 +2,52 @@
 
 import React from 'react';
 import axios from 'axios';
-import { fakeUserData } from './__test__/FakeUsersData';
+import { fakeUsersData } from './__test__/FakeUsersData';
+
+const safuID = process.env.GITHUB_CLIENT_ID; //등록 후 결정
+const safuSecret = process.env.GITHUB_CLIENT_SECRET; // 등록 후 결정
+const redirectUri = 'http://localhost'; //등록 후 결정
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      useremail: '',
+      password: '',
+    };
   }
+
+  handleLoginValue = (key) => (e) => {
+    //입력 값으로 state변경 이벤트
+    this.setState({ [key]: e.target.value });
+  };
+
+  handleLoginButton = () => {
+    //axios post 요청 이벤트
+    axios({
+      method: 'post',
+      url: 'http://localhost/user/login',
+      data: {
+        useremail: this.state.useremail,
+        password: this.state.password,
+      },
+    })
+      .then((res) => {
+        //status 가 200이면,
+        // logged main page로 redirect
+        // App.js의 isLogin state를 state끌어올리기로 변경해주고나서,
+        // history.pushState('/reviews')
+      })
+      .catch((err) => {
+        // if(  ){
+        //status가 401이면 unauthorized error
+        //'That account doesn't existt. Enter a different account or get a new one'띄위기
+        // }
+        // else{
+        //status code가 500이면 server error
+        // }
+      });
+  };
 
   render() {
     return (
@@ -16,18 +55,27 @@ class Login extends React.Component {
         <ul>
           <li>
             <span>email</span>
-            <input type="email"></input>
+            <input type="email" onChange={this.handleLoginValue('useremail').bind(this)}></input>
           </li>
           <li>
             <span>password</span>
-            <input type="password"></input>
+            <input type="password" onChange={this.handleLoginValue('password').bind(this)}></input>
           </li>
         </ul>
         <div>
           <button>Log in</button>
         </div>
         <div>
-          <button>Log in with Github</button>
+          <a
+            href={
+              'https://github.com/login/oauth/authorize?client_id=' +
+              safuID +
+              '&redirect_uri=' +
+              redirectUri
+            }
+          >
+            Log in with Github
+          </a>
         </div>
         <div>
           <button>Find Id</button>
