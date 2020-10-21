031723b4c09583c3d54ee27da429cf231a7c2085
Grace Chung <hjngy0511@gmail.com>
2020-10-21 16:48:59 +0900

    #5/:art:/sign up/sign up server와 API 통신 확인 완료 to do 2 - 유효성 검사에서 fakeUsersData 삭제 후 SignUp API 통해 DB로 real Users Data로 진행 및 정상작동 * 위의 commit message의  SignUp API -> Check UserId API 로 수정합니다. to do 3 - 회원가입 완료 후 SignUp API 통해 Users Data 생성 정상 작동

--- safu-client/src/components/SignUp.js
+++ safu-client/src/components/SignUp.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-import { fakeUsersData } from '../__test__/fakeUsersData';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -22,18 +21,28 @@ class SignUp extends React.Component {
     if (key === 'useremail') {
       var emailreg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
       var useremail = e.target.value;
+      this.setState({ [key]: useremail });
       if (useremail.length > 0 && false === emailreg.test(useremail)) {
         this.setState({ isAvailedEmail: '올바른 이메일 형식이 아닙니다.' });
       } else {
-        for (let userInfo of fakeUsersData) {
-          if (userInfo.useremail === useremail) {
-            this.setState({ isAvailedEmail: '이미 존재하는 email입니다.' });
-            break;
-          } else {
-            this.setState({ isAvailedEmail: '' });
-            this.setState({ [key]: e.target.value });
-          }
-        }
+        axios({
+          method: 'post',
+          url: 'http://localhost:4000/users/signup/checkId',
+          data: {
+            useremail: e.target.value,
+          },
+        })
+          .then((res) => {
+            if (res.data !== null) {
+              this.setState({ isAvailedEmail: '이미 존재하는 email입니다.' });
+            } else {
+              this.setState({ isAvailedEmail: '' });
+              this.setState({ [key]: useremail });
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
       }
     }
     if (key === 'password') {
@@ -61,7 +70,7 @@ class SignUp extends React.Component {
       this.setState({ [key]: e.target.value });
     }
   };
-  handleLoginButton = () => {
+  handleSignUpButton = () => {
     axios({
       method: 'post',
       url: 'http://localhost:4000/users/signup',
@@ -73,7 +82,8 @@ class SignUp extends React.Component {
     })
       .then((res) => {
         //200(OK), 201(Created)
-        this.props.history.push('/users/login');
+        // this.props.history.push('/users/login');
+        console.log('회원가입 완료');
       })
       .catch((err) => {
         //500(err)
@@ -88,35 +98,26 @@ class SignUp extends React.Component {
           <li>
             <label htmlFor="useremail">
               <div>email</div>
-              <input
-                type="useremail"
-                onChange={this.handleSignUpValue('useremail').bind(this)}
-              ></input>
+              <input type="useremail" onChange={this.handleSignUpValue('useremail')}></input>
               <div>{this.state.isAvailedEmail}</div>
             </label>
           </li>
           <li>
             <label htmlFor="password">
               <div>password</div>
-              <input
-                type="password"
-                onChange={this.handleSignUpValue('password').bind(this)}
-              ></input>
+              <input type="password" onChange={this.handleSignUpValue('password')}></input>
               <div>{this.state.isAvailedPassword}</div>
             </label>
           </li>
           <li>
-            <label
-              htmlFor="password check"
-              onChange={this.handleSignUpValue('passwordCheck').bind(this)}
-            >
+            <label htmlFor="password check" onChange={this.handleSignUpValue('passwordCheck')}>
               <div>password 확인</div>
               <input type="password"></input>
               <div>{this.state.isAvailedPasswordCheck}</div>
             </label>
           </li>
           <li>
-            <label htmlFor="Github ID" onChange={this.handleSignUpValue('githubId').bind(this)}>
+            <label htmlFor="Github ID" onChange={this.handleSignUpValue('githubId')}>
               <div>Github ID (for identification)</div>
               <input></input>
             </label>
@@ -126,7 +127,9 @@ class SignUp extends React.Component {
           <button
             onClick={(e) => {
               e.preventDefault();
-              this.handleLoginButton.bind(this);
+              {
+                this.handleSignUpButton();
+              }
             }}
           >
             Submit
