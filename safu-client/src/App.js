765fdb5417c283e7d1bee8884eadc40804bf05cd
hdaleee <hdaleee@kakao.com>
2020-10-22 23:29:48 +0900

    #4/:art:/main befor login / App.js 수정
    1. isLogin state에 따른 분기 설정 완료
    2. menu 컴포넌트 작성 안됨
    3. axios 주석으로 돌려두고 페이크 데이터로 테스트 진행
    4. 82 line ~ 86 line 까지 구현 완료 : 실제 res.data json에 맞추어 세팅함. 확인 필요

--- safu-client/src/App.js
+++ safu-client/src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+// import Menu from "./components/Menu";
+import CardList from "./components/CardList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLogin: true,
+      userInfo: [
+        {
+          id: 3,
+          users_id: 13,
+          bootcamp_id: 5,
+          githublink: "https://github.com/codestates/SAFU-server.git",
+          price: "비쌈",
+          level: "어려움",
+          recommend: "추천",
+          curriculum: "어려움",
+          comment: "자기주도 학습!!!!중심이다",
+          active: true,
+          createdAt: "2020-10-17T13:00:04.000Z",
+          updatedAt: "2020-10-17T13:00:04.000Z",
+          useremail: {
+            email: "thdguswn93@naver.com",
+          },
+          bootcampname: {
+            name: "codestates",
+          },
+        },
+        {
+          id: 4,
+          users_id: 15,
+          bootcamp_id: 7,
+          githublink: "https://sequelize.org/master/manual/eager-loading.html",
+          price: "비쌈",
+          level: "어려움",
+          recommend: "추천",
+          curriculum: "어려움",
+          comment: "없음",
+          active: true,
+          createdAt: "2020-10-18T16:00:00.000Z",
+          updatedAt: "2020-10-18T16:00:00.000Z",
+          useremail: {
+            email: "hun3780@gmail.com",
+          },
+          bootcampname: {
+            name: "fastcampus",
+          },
+        },
+        {
+          id: 5,
+          users_id: 13,
+          bootcamp_id: 7,
+          githublink: "www.naver.com",
+          price: "q",
+          level: "d",
+          recommend: "d",
+          curriculum: "d",
+          comment: "d",
+          active: true,
+          createdAt: "2020-10-18T17:00:00.000Z",
+          updatedAt: "2020-10-18T17:00:00.000Z",
+          useremail: {
+            email: "thdguswn93@naver.com",
+          },
+          bootcampname: {
+            name: "fastcampus",
+          },
+        },
+      ],
+    };
+  }
+
+  handleIsLoginChange() {
+    this.setState({ isLogin: true });
+    // axios.get("http://localhost:4000/user").then((res) => {
+    //   console.log(res.data);
+    //   this.setState({ isLogin: res.data[1] });
+    //   this.setState({ userInfo: res.data[0] });
+    // });
+  }
+
+  render() {
+    console.log("this.state:", this.state);
+    if (this.isLogin) {
+      return (
+        <div>
+          {/* Nav component */}
+          <Nav
+            isLogin={this.state.isLogin}
+            handleIsLoginChange={this.handleIsLoginChange.bind(this)}
+          />
+
+          {/* main component */}
+          <div className="main-body">
+            {/* <Menu /> */}
+            <CardList isLogin={this.state.isLogin} userInfo={this.state.userInfo} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {/* Nav component */}
+          <Nav
+            isLogin={this.state.isLogin}
+            handleIsLoginChange={this.handleIsLoginChange.bind(this)}
+          />
+
+          {/* main component */}
+          <div className="main-body">
+            {/* {/* <Menu /> */}
+            <CardList isLogin={this.state.isLogin} userInfo={this.state.userInfo} />
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
