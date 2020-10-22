4f1388c7b2f8a6cc1df1df825d97285301376b2c
hdaleee <hdaleee@kakao.com>
2020-10-22 23:33:37 +0900

    #4/:art:/main befor login / Nav 컴포넌트 작성 완료
    1. Login 컴포넌트와 SignUp 컴포넌트 주석처리해둠 : line 4 ~ 5
    2. line 11에 따라 <li>로 처리해 둔 코드를 컴포넌트로 변경해줘서 연동해야함
    3. Mypage와 Logout 또한 추후 작업해야함

--- safu-client/src/components/Nav.js
+++ safu-client/src/components/Nav.js
@@ -1,1 +1,44 @@
 //Nav.js - (state에 따라 or 라우팅에 따라) 변경되는 부분: signup, login, mypage, logout 버튼
+import React from "react";
+import axios from "axios";
+// import Login from "./Login";
+// import Signup from "./SignUp";
+
+axios.defaults.withCredentials = true;
+
+function Nav(props) {
+  // console.log("props :",props);
+  // 하위의 li 들은 컴포넌트로 변경예정
+  if (props.isLogin) {
+    return (
+      <div>
+        <h2 className="title">S*FU</h2>
+        <ul>
+          <li>
+            <a href="">my page</a>
+          </li>
+          <li>
+            <a href="">log out</a>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2 className="title">S*FU</h2>
+        {/* 하위 컴포넌트에 props.handleIsloninChange 를 내려줘야함 */}
+        <ul>
+          <li>
+            <a href="">sign up</a>
+          </li>
+          <li>
+            <a href="">log in</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
