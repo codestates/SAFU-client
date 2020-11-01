0fc4a462460451ff202d0bee14c54eabcd3c8379
Grace Chung <hjngy0511@gmail.com>
2020-11-01 11:20:19 +0900

    #80/:art:/Routing/CardWrite후 리다이렉트 window.location은 페이지 전체 로딩에 필요한 것인데 이 경우는 Main 컴포넌트만 리로딩 되면 되기 때문에 this.props.history.push(/) 사용

--- safu-client/src/components/CardList.js
+++ safu-client/src/components/CardList.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-
-import { CardWrite, Card } from '../pages';
-
-//CardList.js - (state에 따라 or 라우팅에 따라) 변경되는 부분:
-//  1. 비로그인시 main => 메뉴에서 특정 bootcamp click시 랜더링 되는 Card 컴포넌트들
-//  2. 로그인지 main => + 버튼(누르면 CardWrite 컴포넌트가 있는 페이지로 감)추가로 랜더링 되고 Card 컴포넌트들이 이어서 랜더링
-//  3. Mypage => 자신이 쓴 Card 컴포넌트들만 랜더링
+import { Link } from 'react-router-dom';
+import { Card } from '../pages';
 
 class CardList extends React.Component {
   constructor(props) {
