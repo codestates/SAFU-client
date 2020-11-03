4919213d91733315f4f4c48ae4c2d63156a2bfc2
hdaleee <hdaleee@kakao.com>
2020-11-03 20:56:05 +0900

    #19/CSS/githubLinkCSS
    1. App.css : 깃허브링크 ...으로 생략되도록 처리
    2. DELETE 버튼 X 버튼으로 변경 및 위치 & z-index 값 조정
    3. CardEditList.js : 수정안내문구 추가

--- safu-client/src/components/CardEditList.js
+++ safu-client/src/components/CardEditList.js
@@ -6,6 +6,9 @@ function CardEditList(userInfo) {
 
   return (
     <div className="cardEdit-list">
+      <p className="cardEdit-list-before-message">
+        아래 카드에서 바로 수정하고 SUMMIT을 눌러 반영하세요!
+      </p>
       {mypageCardList.map((card) => (
         <CardEdit key={card.id} card={card} />
       ))}
